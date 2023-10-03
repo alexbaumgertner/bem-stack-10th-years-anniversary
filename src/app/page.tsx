4a5cd243b40aca11c5dc8835b7f0bc1cf557ab9e
Alex Baumgertner <alex.baumgertner@gmail.com>
2023-10-03 13:23:34 +0000

    Add table of content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,51 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      Полный стек БЭМ
-    </main>
+    <div className="page">
+      <main>
+        БЭМ десять лет спустя
+      </main>
+
+      <menu>
+        <ul>
+          <li>
+            <Link href="/slides/02-what-is-bem">
+              Кратко о БЭМ: история, основные понятия
+            </Link>
+          </li>
+          <li>
+            <Link href="/slides/">
+              Шаблонизация и шаблоны: XSLT, BEMXJST и другие (server side React)
+            </Link>
+          </li>
+          <li>
+            <Link href="/slides/">
+              Клиентский JS: jQuery, i-bem, BackboneJS, React, Svetle
+            </Link>
+          </li>
+          <li>
+            <Link href="/slides/">
+              Библиотеки блоков
+            </Link>
+          </li>
+          <li>
+            <Link href="/slides/">
+              Инфраструктура: bem-tools, webpack
+            </Link>
+          </li>
+          <li>
+            <Link href="/slides/">
+              БЭМ в 2023 году
+            </Link>
+          </li>
+          <li>
+            <Link href="/slides/">
+              Ссылки, материалы, доклады, видео
+            </Link>
+          </li>
+        </ul>
+      </menu>
+    </div>
   )
 }
